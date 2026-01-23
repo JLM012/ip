6639b6cc003c9c1e5dad443303aa455131cd6c66
Jasher <jasherlim32@gmail.com>
2026-01-23 18:14:38 +0800

    Add echo behaviour

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -1,10 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Spark {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Spark!");
         System.out.println("How can i help you today?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye! Hope to see you again soon!");
 
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("It was a pleasure assisting you! Goodbye!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
