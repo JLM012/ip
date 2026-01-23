7d8028b3b66d489d6256f6d458ad082fd8894264
Jasher <jasherlim32@gmail.com>
2026-01-23 22:20:44 +0800

    Move logic out of main class

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -1,25 +1,7 @@
-import java.io.*;
-import java.util.Scanner;
-
+//Entry point of the chatbot.
+//Execution logic is done in SparkApp.
 public class Spark {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Spark!");
-        System.out.println("How can i help you today?");
-        System.out.println("____________________________________________________________");
-
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("It was a pleasure assisting you! Goodbye!");
-                System.out.println("____________________________________________________________");
-                break;
-            }
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
-        }
+        new SparkApp().start();
     }
 }
