ae3f14b35ed4794648dbeeb6f68e161de030de52
Jasher <jasherlim32@gmail.com>
2026-01-30 21:21:54 +0800

    Change class structure

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -1,7 +1,119 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 //Entry point of the chatbot.
-//Execution logic is done in SparkApp.
 public class Spark {
+    private Ui ui = new Ui();
+    private TaskList tasks;
+    private final Storage storage = new Storage(Paths.get("data", "spark.txt"));
+    private boolean didLoadFail = false;
+    private String loadFailMessage = "";
+
+    public Spark() {
+        try {
+            List<Task> loadedTasks = storage.load();
+            this.tasks = new TaskList(loadedTasks);
+        } catch (SparkException e) {
+            this.tasks = new TaskList();
+            didLoadFail = true;
+            loadFailMessage = e.getMessage();
+        }
+    }
+
+    public void start() {
+        ui.showWelcome();
+
+        if (didLoadFail) {
+            ui.showError("Saved tasks could not be loaded, reverting to empty list");
+        }
+
+        // reads user input and adds to tasklist.
+        // command "list" displays the current list.
+        // command "mark" or "unmark" marks/unmark task at specified index
+        // Tasks can be added using command "todo", "deadline", "event"
+        // command "bye" exits the chatbot
+        while (true) {
+            String input = ui.readCommand();
+
+            if (input.equals("bye")) {
+                ui.showBye();
+                break;
+            }
+            try {
+                handleInput(input);
+            } catch (SparkException e) {
+                ui.showError(e.getMessage());
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        new SparkApp().start();
+        new Spark().start();
+    }
+
+    private void handleInput(String input) throws SparkException {
+        String[] args = input.split(" ", 2);
+        String command = args[0];
+        String rest = (args.length == 2) ? args[1] : "";
+
+        if (command.equals("list")) {
+            ui.showList(tasks);
+
+        } else if (command.equals("mark")) {
+            int index = Integer.parseInt(args[1]) - 1;
+            Task marked = tasks.mark(index);
+            ui.showMark(marked);
+            storage.save(tasks);
+
+        } else if (command.equals("unmark")) {
+            int index = Integer.parseInt(args[1]) - 1;
+            Task unmarked = tasks.unmark(index);
+            ui.showUnmark(unmarked);
+            storage.save(tasks);
+
+        } else if (command.equals("todo")) {
+            if (rest.isEmpty()) {
+                throw new SparkException("Please provide a descrioption for todo. Example: todo read book");
+            }
+            Task todo = new Todo(rest);
+            tasks.addTask(todo);
+            int totalTasks = tasks.getSize();
+            ui.showAdded(todo, totalTasks);
+            storage.save(tasks);
+
+        } else if (command.equals("deadline")) {
+            String[] deadlineArgs = rest.split(" /by ", 2);
+            String description = deadlineArgs[0];
+            String by = deadlineArgs[1];
+            Task deadline = new Deadline(description, by);
+            tasks.addTask(deadline);
+            int totalTasks = tasks.getSize();
+            ui.showAdded(deadline, totalTasks);
+            storage.save(tasks);
+
+        } else if (command.equals("event")) {
+            String[] eventArgs = rest.split(" /from ", 2);
+            String description = eventArgs[0];
+            String[] fromTo = eventArgs[1].split(" /to ", 2);
+            String from = fromTo[0];
+            String to = fromTo[1];
+            Task event = new Event(description, from, to);
+            tasks.addTask(event);
+            int totalTasks = tasks.getSize();
+            ui.showAdded(event, totalTasks);
+            storage.save(tasks);
+
+        } else if (command.equals("delete")) {
+            int index = Integer.parseInt(args[1]) - 1;
+            Task deleted = tasks.deleteTask(index);
+            int totalTasks = tasks.getSize();
+            ui.showDeleted(deleted, totalTasks);
+            storage.save(tasks);
+
+        } else {
+            throw new SparkException("The input you provided is invalid");
+        }
+
     }
 }
