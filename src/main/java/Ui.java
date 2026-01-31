45d0917b89c78691091cf929e612b94f2a9125ca
Jasher <jasherlim32@gmail.com>
2026-01-31 11:45:04 +0800

    Change Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -6,67 +6,71 @@ public class Ui {
 
     //prints welcome message at the start of chatbot.
     public void showWelcome() {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("Hello! I'm Spark!");
         System.out.println("How can i help you today?");
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints a confirmation message after a task was added.
     public void showAdded(Task task, int totalTasks) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("Done. I've added this task:" );
         System.out.println("   " + task);
         System.out.println("Now you have " + totalTasks + " tasks in the list" );
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints the current list of tasks after command "list" is given.
     public void showList(TaskList tasks) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.getSize(); i++) {
             System.out.println((i + 1) + ". " + tasks.getTask(i));
         }
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints exit message after command "bye" is given.
     public void showBye() {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("It was a pleasure assisting you! Goodbye!");
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints marked tast specified
     public void showMark(Task task) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("   " + task);
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints unmarked task specified
     public void showUnmark(Task task) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("   " + task);
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //print error messages
     public void showError(String message) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println(message);
-        System.out.println("____________________________________________________________");
+        printSeparator();
     }
 
     //prints a confirmation message after a task was deleted.
     public void showDeleted(Task task, int totalTasks) {
-        System.out.println("____________________________________________________________");
+        printSeparator();
         System.out.println("Done. I've deleted this task:" );
         System.out.println("   " + task);
         System.out.println("Now you have " + totalTasks + " tasks in the list" );
+        printSeparator();
+    }
+
+    public void printSeparator() {
         System.out.println("____________________________________________________________");
     }
 
